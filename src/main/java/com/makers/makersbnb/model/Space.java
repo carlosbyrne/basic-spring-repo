e3ba266d9b83dc54798addbc77aa9a756e0848e6
carlosbyrne <carlosbyrne@outlook.com>
2026-03-06 12:18:59 +0000

    got lombok getter and setters to work by changing dependencies in build.gradle to 'org.projectlombok:lombok:1.18.30'

--- src/main/java/com/makers/makersbnb/model/Space.java
+++ src/main/java/com/makers/makersbnb/model/Space.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 // @Entity - instances of this class map to database records
 @SuppressWarnings("ALL")
@@ -19,13 +21,25 @@ public class Space {
     // the value of id is generated automatically
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     // a second field that holds the name of each space
+    @Getter
+    @Setter
     private String name;
+
     // a third field storing the description of each space
+    @Getter
+    @Setter
     private String description;
+
     // a third field storing the price of each space
+    @Getter
+    @Setter
     private Double price;
+
     // a fourth field storing the rules
+    @Getter
+    @Setter
     private String rules;
 
     // a zero-arguments constructor
@@ -37,18 +51,7 @@ public class Space {
         this.name = name;
     }
 
-    // get and set methods
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    public Double getPrice() { return price; }
-    public void setPrice(Double price) { this.price = price; }
-
-    public String getRules() { return rules; }
-    public void setRules(String rules) { this.rules = rules; }
 }
 
 
